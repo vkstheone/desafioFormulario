fd004373beab91d4d788b83d45f9870cbe42ba36
vkstheone <89942870+vkstheone@users.noreply.github.com>
2024-09-19 20:02:29 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,67 @@ import { useState } from 'react';
 // * Não é permitido usar refs
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-// todo - Desabilite o botão de Login equanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// # - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// # - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// # - Desabilite o botão de Login equanto você está executando o login.
+// # - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// # - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+  const[error, setError] = useState(null);
+  const[isRequesting, setIsRequesting] = useState(false);
+  
+  const handleEmail = (e) => {
+      const value = e.target.value;
+      console.log(value)
+
+      setEmail(value)
+  }
+  const handlePassword = (e) => {
+      const value = e.target.value;
+      console.log(value)
+
+      setPassword(value)
+  }
+
+  const handleSubmit = () =>{
+    console.log('submit')
+
+    setError(null)
+
+    setIsRequesting(true)
+
+    let values = { email: email, password: password};
+    login(values)
+      .then(() => {
+        alert("login feito com sucesso")
+      })
+      .catch((error) => {
+      console.log(error)
+      setError(error)}).finally(setIsRequesting(false));
+  }
+
+
   return (
     <div className='wrapper'>
       <div className='login-form'>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        {error && <div className='errorMessage'>{error.message}</div>}
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input id={'email'} type={'email'} autoComplete='off' value={email} onChange={handleEmail} />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input id={'password'} type={'password'} value={password} onChange={handlePassword} />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          <button onClick={handleSubmit} disabled={email == '' || password.length < 6 || isRequesting } >Login</button>
         </div>
       </div>
     </div>
